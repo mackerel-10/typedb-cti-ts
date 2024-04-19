846b428127290cafa74f07e6ebc14dcf3f0cd4bc
Sujin Jeon <sjjeon@enki.co.kr>
2024-04-19 14:40:04 +0900

    Feat: define schemas

--- src/schema/initialize-typedb.ts
+++ src/schema/initialize-typedb.ts
@@ -1,18 +1,60 @@
-import { TypeDB, TypeDBDriver } from 'typedb-driver';
+import {
+  SessionType,
+  TransactionType,
+  TypeDB,
+  TypeDBDriver,
+  TypeDBSession,
+  TypeDBTransaction,
+} from 'typedb-driver';
+import fs from 'fs';
 
 const initializeTypedb = async (
   typeDBUri: string,
   database: string,
 ): Promise<TypeDBDriver> => {
-  // #1 Make driver
-  const driver: TypeDBDriver = await TypeDB.coreDriver(typeDBUri);
+  let driver: TypeDBDriver | undefined;
+  let session: TypeDBSession | undefined;
+  let transaction: TypeDBTransaction | undefined;
 
-  const isDatabaseExist: boolean = await driver.databases.contains(database);
-  if (!isDatabaseExist) {
-    await driver.databases.create(database);
+  try {
+    // #1 Make driver
+    driver = await TypeDB.coreDriver(typeDBUri);
+
+    const isDatabaseExist: boolean = await driver.databases.contains(database);
+    if (!isDatabaseExist) {
+      await driver.databases.create(database);
+    }
+
+    // #2 Define cti-schema.tql
+    const ctiSchema: string = fs.readFileSync(
+      './src/schema/cti-schema.tql',
+      'utf8',
+    );
+    /*const ctiRules: string = fs.readFileSync(
+      './src/schema/cti-rules.tql',
+      'utf8',
+    );*/
+
+    session = await driver.session(database, SessionType.SCHEMA);
+    transaction = await session.transaction(TransactionType.WRITE);
+
+    // Cti Schema
+    await transaction.query.define(ctiSchema);
+    await transaction.commit();
+
+    // Cti Rules
+    /*await transaction.query.define(ctiRules);
+    await transaction.commit();*/
+  } catch (error) {
+    console.error(error);
+  } finally {
+    if (transaction?.isOpen()) await transaction.close();
+    await session?.close();
   }
 
-  // #2 Define cti-schema.tql
+  if (!driver) {
+    throw new Error('TypeDB driver is not initialized');
+  }
 
   return driver;
 };
